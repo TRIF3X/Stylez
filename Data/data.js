10564ea915fe5ac30a75abab8768d25a10fb363d
TRIF3X <thewizardofcrypto@gmail.com>
2019-04-04 09:41:59 -0500

    init seeding some data

---
+++ Data/data.js
@@ -0,0 +1,65 @@
+//All products from H&M website
+
+let mens = [
+    {
+        category: 'shirts',
+        name: 'Regular Fit Linen-blend Shirt',
+        price: 19.99,
+        description: 'Shirt in an airy linen and cotton blend. Turn-down collar, classic front with button placket, and yoke at back. Open chest pocket and long sleeves with adjustable buttoning at cuffs. Rounded hem, slightly longer at back. Regular fit – classic fit with good room for movement and gently shaped waist for a comfortable, tailored silhouette.',
+        imageUrl: 'https://lp2.hm.com/hmgoepprod?set=source[/6a/4d/6a4d855a7dcc402beb5ec034dd2fd5218186db32.jpg],origin[dam],category[men_shirts_longsleeved],type[LOOKBOOK],res[m],res[m],hmver[1]&call=url[file:/product/main]'
+
+    },
+    {
+        category: 'shirts',
+        name: 'Premium Cotton Shirt',
+        price: 14.99,
+        description: 'Long-sleeved shirt in woven fabric with an easy-iron finish. Turn-down collar, classic button placket, and darts and yoke at back. Narrow cuffs with adjustable buttoning. Rounded hem. Slim fit with narrow shoulders and tapered waist.',
+        imageUrl: 'https://lp2.hm.com/hmgoepprod?set=source[/01/78/01789228b79d929627675759cb9e08ed95343de1.jpg],origin[dam],category[men_shirt_dressed_slimfit],type[LOOKBOOK],res[m],hmver[1]&call=url[file:/product/main]'
+
+    },
+    {
+        category: 'shirts',
+        name: 'Slim Fit Viscose Shirt',
+        price: 17.99,
+        description: 'Shirt in airy, woven viscose fabric with a turn-down collar. Smooth button placket, long sleeves with adjustable buttoning at cuffs, and yoke at back. Rounded hem. Slim fit – narrow shoulders and tapered waist for a tailored silhouette.',
+        imageUrl: 'https://lp2.hm.com/hmgoepprod?set=source[/48/ad/48adba638c7aec94e6f0488c2ec78d229a09a96e.jpg],origin[dam],category[men_shirt_dressed_slimfit],type[LOOKBOOK],res[m],res[m],hmver[1]&call=url[file:/product/main]'
+
+    },
+    {
+        category: 'shirts',
+        name: 'Oxford Shirt Regular fit',
+        price: 24.99,
+        description: 'CONSCIOUS. Shirt in organic cotton oxford with a button-down collar. Open chest pocket, long sleeves with buttons at cuffs, and yoke with locker loop at back. Rounded hem. Regular fit – a classic fit with good room for movement and gently shaped waist for a comfortable, tailored silhouette.',
+        imageUrl: 'https://lp2.hm.com/hmgoepprod?set=source[/f7/14/f714ee42df067b3b40151ad0b954280e5f082e7b.jpg],origin[dam],category[men_shirt_dressed_regularfit],type[LOOKBOOK],res[m],res[m],hmver[1]&call=url[file:/product/main]'
+
+    },
+    {
+        category: 'shirts',
+        name: 'Regular Fit Collarless Shirt',
+        price: 24.99,
+        description: 'Collarless shirt in woven cotton fabric. Open chest pocket, classic button placket, long sleeves with cuffs, and yoke at back. Rounded hem. Regular fit – a classic fit with good room for movement and a gently shaped waist for a comfortable, tailored silhouette.',
+        imageUrl: 'https://lp2.hm.com/hmgoepprod?set=source[/bc/bb/bcbb070b8731a48eafe885f997be7eb76357bda7.jpg],origin[dam],category[men_shirts_casual],type[LOOKBOOK],res[m],res[m],hmver[1]&call=url[file:/product/main]'
+
+    },
+    {
+        category: 'shirts',
+        name: 'Slim Fit Shirt',
+        price: 14.99,
+        description: 'Short-sleeved shirt in woven cotton-blend fabric. Turn-down collar, buttons without placket, and rounded hem. Slim Fit – narrow shoulders and tapered waist for a tailored silhouette.',
+        imageUrl: 'https://lp2.hm.com/hmgoepprod?set=source[/49/39/4939d7f123025d02f7460f0e594547836ad23b56.jpg],origin[dam],category[men_shirts_casual],type[LOOKBOOK],res[m],res[m],hmver[1]&call=url[file:/product/main]'
+
+    },
+]
+
+let womans = [
+    {
+
+    }
+]
+
+module.exports = {
+    mens,
+    womans
+}
+
+
